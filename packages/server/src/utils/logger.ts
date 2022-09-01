89e7c8924f51506596017ed5a620de52edfae408
Nuno Lopes <nuno.lopes@fraunhofer.pt>
2022-09-01 16:05:40 +0100

    Removed unused files. Fixed merge

--- packages/server/src/utils/logger.ts
+++ packages/server/src/utils/logger.ts
@@ -1,20 +1,26 @@
 import log4js from 'log4js'
 
+const apiCallsLayout = {
+  type: 'pattern',
+  pattern: '[%d] %m'
+}
+
 log4js.configure({
   appenders: {
-    stdout: { type: 'stdout' },
-    file: { type: 'file', filename: 'logs/server.log' },
-    stdoutFilter: {
-      type: 'logLevelFilter', appender: 'stdout', level: 'all'
-    },
-    fileFilter: {
-      type: 'logLevelFilter', appender: 'file', level: 'all'
+    serverConsole: { type: 'stdout' },
+    serverFile: { type: 'file', filename: 'logs/server.log' },
+    apiCallsFile: {
+      type: 'dateFile',
+      pattern: 'yyyy-MM',
+      filename: 'logs/api_calls.log',
+      keepFileExt: true,
+      alwaysIncludePattern: true,
+      layout: apiCallsLayout
     }
   },
   categories: {
-    default: {
-      appenders: ['stdoutFilter', 'fileFilter'], level: 'all'
-    }
+    default: { appenders: ['serverFile', 'serverConsole'], level: 'all' },
+    api_call: { appenders: ['apiCallsFile'], level: 'all' }
   }
 })
 
