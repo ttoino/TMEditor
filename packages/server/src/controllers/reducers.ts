699d6d392c80058e59179511d36a8933ae976968
toino <joaoapereira21@hotmail.com>
2023-04-06 11:46:43 +0100

    Implement editor backend for the reducers

---
+++ packages/server/src/controllers/reducers.ts
@@ -0,0 +1,27 @@
+import fs from 'fs/promises'
+import { Request, Response } from 'express'
+import { REDUCERS_PATH } from '@app/constants/config-file-paths'
+
+export const get = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const reducers = await fs.readFile(`${REDUCERS_PATH}index.js`)
+
+    res.status(200).type('.js').send(reducers)
+  } catch (error) {
+    res.status(200).type('.js').send('')
+  }
+}
+
+export const put = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const reducers = req.body
+
+    console.log(reducers)
+
+    await fs.writeFile(`${REDUCERS_PATH}index.js`, reducers)
+
+    res.status(200).type('.js').send(reducers)
+  } catch (error) {
+    res.status(500)
+  }
+}
--- packages/server/src/routes/editor.ts
+++ packages/server/src/routes/editor.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express'
 import { put as configController } from '@app/controllers/config'
 import { put as pagesController } from '@app/controllers/pages'
+import * as reducersController from '@app/controllers/reducers'
 
 export default (router: Router) => {
   router.put('/config', configController)
   router.put('/pages/:page?', pagesController)
+  router.get('/reducers', reducersController.get)
+  router.put('/reducers', reducersController.put)
 }
