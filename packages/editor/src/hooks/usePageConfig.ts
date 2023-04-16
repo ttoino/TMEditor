32f9aaf66d7f25956d824522cb4b64a985367638
toino <joaoapereira21@hotmail.com>
2023-04-16 22:05:11 +0100

    Remove data and error info from page response

--- packages/editor/src/hooks/usePageConfig.ts
+++ packages/editor/src/hooks/usePageConfig.ts
@@ -1,12 +1,35 @@
 import { putPage } from "@app/api";
 import { getPage } from "@common/api";
-import type { PageResponse } from "@types";
+import type { Page, UIComponent, UIComponentResponse } from "@types";
 import useLocalState from "./useLocalState";
 
+const removeData = (component: UIComponentResponse) => {
+    // @ts-ignore
+    if (component.components) {
+        // @ts-ignore
+        component.components = component.components.map(removeData);
+    }
+
+    // @ts-ignore
+    delete component.data;
+    // @ts-ignore
+    delete component.meta;
+    delete component.error;
+
+    return component as UIComponent;
+};
+
 const usePageConfig = (page: string | undefined) =>
-    useLocalState<PageResponse>(
+    useLocalState<Page>(
         ["page", page],
-        () => getPage(page, new URLSearchParams()),
+        async () => {
+            const config = await getPage(page, new URLSearchParams());
+
+            // @ts-ignore
+            config.components = config.components.map(removeData);
+
+            return config as Page;
+        },
         (config) => putPage(page, config)
     );
 
