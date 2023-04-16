2d8c252ce7f9981478d976b50bbe8c95670f3144
toino <joaoapereira21@hotmail.com>
2023-04-16 22:06:17 +0100

    Simplify StyledButton

--- packages/editor/src/components/StyledButton.tsx
+++ packages/editor/src/components/StyledButton.tsx
@@ -1,26 +1,11 @@
-import React from "react";
+import { styled } from "@common/theme";
 
-type Props = {
-    text: String,
-    onClick: () => void
-}
-
-
-const StyledButton = ({text, onClick}: Props) => {
-
-    const buttonStyle = {
-        padding: "0.3em 0.8em",
-        fontSize: "1em",
-        border: "none",
-        borderRadius: "0.3em",
-        backgroundColor:  "#007eb2",
-        color: "white",
-        cursor: "pointer",
-    };
-
-
-    return (
-        <button style={buttonStyle} onClick={onClick}> {text} </button>
-    );
-}
-export default StyledButton;
+export default styled("button", {
+    padding: "0.3em 0.8em",
+    fontSize: "1em",
+    border: "none",
+    borderRadius: "0.3em",
+    backgroundColor: "$primary",
+    color: "white",
+    cursor: "pointer",
+});
