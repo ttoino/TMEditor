78ac163bac320e4bf734d861517733c47642c470
rootie <root@mrocha31415.localdomain>
2023-04-28 13:48:22 +0100

    initial commit - value form

--- packages/editor/src/components/forms/ValueForm.tsx
+++ packages/editor/src/components/forms/ValueForm.tsx
@@ -1,12 +1,31 @@
 import React from "react";
 import type { Value } from "@types";
 import { UpdateFn } from "@app/hooks/useLocalState";
+import FormComponent from "../FormComponent";
+import updateAt from "@app/util/updateAt";
 
 interface Props {
     component: Value;
     update: UpdateFn<Value>;
 }
 
+
 export default function ValueForm({ component, update }: Props) {
-    return <></>;
+    return <>
+        <FormComponent
+            component="input"
+            label="Title"
+            type="text"
+            value={component.title}
+            onValueChange={updateAt(update, "title")}
+            ></FormComponent>
+
+        <FormComponent
+            component="input"
+            label="Precision"
+            type="number"
+            value={component.precision}
+            onValueChange={updateAt(update, "precision")}
+            ></FormComponent>
+    </>;
 }
