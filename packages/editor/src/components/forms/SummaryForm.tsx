c07328efc5335e82a6eb26121d69bd8414c72d03
Milena Gouveia <up202008862@edu.fe.up.pt>
2023-04-28 11:52:56 +0100

    Add title and precision input of SummaryForm

--- packages/editor/src/components/forms/SummaryForm.tsx
+++ packages/editor/src/components/forms/SummaryForm.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import type { Summary } from "@types";
 import { UpdateFn } from "@app/hooks/useLocalState";
+import FormComponent from "../FormComponent";
+import updateAt from "@app/util/updateAt";
 
 interface Props {
     component: Summary;
@@ -8,5 +10,28 @@ interface Props {
 }
 
 export default function SummaryForm({ component, update }: Props) {
-    return <></>;
+    const updateTitle = updateAt(update, "title");
+    const updatePrecision = (value: any) => updateAt(update, "precision")(parseInt(value));
+
+    return (
+        <>
+            <FormComponent
+                component="input"
+                label="Title"
+                value={component.title}
+                onValueChange={updateTitle}
+            >
+            </FormComponent>
+
+            <FormComponent
+                component="input"
+                type="number"
+                label="Precision"
+                value={component.precision}
+                onValueChange={updatePrecision}
+            >
+            </FormComponent>
+
+        </>
+    );
 }
