ba1b791637312f10534daf98821c58acb696a2b3
toino <joaoapereira21@hotmail.com>
2023-05-18 08:17:29 +0100

    Add form for columns component

--- packages/editor/src/components/forms/ColumnsForm.tsx
+++ packages/editor/src/components/forms/ColumnsForm.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import type { Columns } from "@types";
 import { UpdateFn } from "@app/hooks/useLocalState";
+import { styled } from "@common/theme";
+import ComponentList from "../ComponentList";
+import updateAt from "@app/util/updateAt";
+import IconButton from "../IconButton";
+import { MdAdd } from "react-icons/md";
+import appendAt from "@app/util/appendAt";
 
 interface Props {
     component: Columns;
@@ -8,5 +14,50 @@ interface Props {
 }
 
 export default function ColumnsForm({ component, update }: Props) {
-    return <></>;
+    return (
+        <>
+            <StyledListTitle>
+                Children
+                <IconButton
+                    onClick={() =>
+                        appendAt(
+                            update,
+                            "components"
+                        )({
+                            _open: true,
+                            type: "heading",
+                        })
+                    }
+                >
+                    <MdAdd />
+                </IconButton>
+            </StyledListTitle>
+
+            <Wrapper>
+                <ComponentList
+                    components={component.components}
+                    update={updateAt(update, "components")}
+                />
+            </Wrapper>
+        </>
+    );
 }
+
+const Wrapper = styled("div", {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "start",
+    gap: "$1",
+
+    "& > *": {
+        flex: 1,
+    },
+});
+
+const StyledListTitle = styled("div", {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: "$1",
+});
